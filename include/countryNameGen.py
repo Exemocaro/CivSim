19a8cf55b0d50e254098e2542b86d9a8a83bee7e
Cavalex <mateusmpta@gmail.com>
2021-06-05 18:57:41 +0100

    updated requirements.txt and countryNameGen file

--- include/countryNameGen.py
+++ include/countryNameGen.py
@@ -1,11 +1,11 @@
-# Run this file for some time if you want some new names for your nations.
-# New names will be placed on ../data/kingdomNames.txt
+# run this file for some time if you want some new names for your nations.
+# run this file on this directory or it won't find the kingdomNames.txt file
+# new names will be placed on ../data/kingdomNames.txt
 
 from requests_html import HTMLSession
-names = open("../data/kingdomNames.txt", "a")
-session = HTMLSession()
+import os
 
-def get10CountryNames():
+def get10CountryNames(names, session):
 
     #define our URL
     url = 'https://www.fantasynamegenerators.com/kingdom-names.php'
@@ -13,7 +13,7 @@ def get10CountryNames():
     #use the session to get the data
     r = session.get(url)
 
-    #Render the page, up the number on scrolldown to page down multiple times on a page
+    #render the page, up the number on scrolldown to page down multiple times on a page
     r.html.render(sleep=0.5, keep_page=True, scrolldown=1)
 
     #take the rendered html and find the element that we are interested in
@@ -39,5 +39,21 @@ def get10CountryNames():
 
     return kingdoms
 
-while True:
-    print(get10CountryNames())
+def main():
+
+    namesDir = ["../data/kingdomNames.txt", "kingdomNames.txt"]
+    fileOpening = ["a", "w"]
+    selection = 0
+
+    # check if the file exists before opening it, and changing the file location if it doesn't exist
+    if not os.path.isfile(namesDir[selection]):
+        print(f"\n{namesDir[selection]} not found! Will create a new file and change the directory from {namesDir[selection]} to {namesDir[selection + 1]}...")
+        selection = 1
+    with open(namesDir[selection], fileOpening[selection]) as names:
+        session = HTMLSession()
+
+        while True:
+            print(get10CountryNames(names, session))
+
+if __name__ == "__main__":
+    main()
